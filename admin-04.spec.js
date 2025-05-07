4b78ee5b1933055f8bba0096498d210865d65d4c
nhantrinh <nhantrinh989@gmail.com>
2025-05-07 21:19:35 +0700

    Update admin-04.spec.js

--- admin-04.spec.js
+++ admin-04.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Validate Category Dropdown', async ({ page }) => {
   // Log in as admin
-  await page.goto('http://localhost:5713/admin');
+  await page.goto('http://localhost:5173/admin');
   await page.fill('input[name="username"]', 'admin');
   await page.fill('input[name="password"]', 'password');
   await page.click('button[type="submit"]');
