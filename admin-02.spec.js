fd5b07904943af0dc6f55b6ea95a45e1b5faf0c1
nhantrinh <nhantrinh989@gmail.com>
2025-05-07 21:18:04 +0700

    Update admin-02.spec.js

--- admin-02.spec.js
+++ admin-02.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Submit Article with Valid Data', async ({ page }) => {
   // Log in as admin
-  await page.goto('http://localhost:5713/admin');
+  await page.goto('http://localhost:5173/admin');
   await page.fill('input[name="username"]', 'admin');
   await page.fill('input[name="password"]', 'password');
   await page.click('button[type="submit"]');
